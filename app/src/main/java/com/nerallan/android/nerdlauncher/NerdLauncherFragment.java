9a0520b1a287ef5a88527afcb858b4c2deac0cb8
Nerallan <budkomaximidu@gmail.com>
2018-11-24 02:23:51 +0300

    update RecyclerView list, filled with activity tags(names of apps)

--- app/src/main/java/com/nerallan/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/nerallan/android/nerdlauncher/NerdLauncherFragment.java
@@ -1,20 +1,30 @@
 package com.nerallan.android.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by Nerallan on 11/22/2018.
  */
 
 public class NerdLauncherFragment extends Fragment {
+    private static final String TAG = "NerdLauncherFragment";
 
     private RecyclerView mRecyclerView;
 
@@ -29,6 +39,88 @@ public class NerdLauncherFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
         mRecyclerView = (RecyclerView)view.findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
         return view;
+    }
+
+    private void setupAdapter(){
+        // Intent i = new Intent(Intent.ACTION_SEND);
+        // i = Intent.createChooser(i, getString(R.string.send_report));
+        // startActivity(i)
+
+        // The MAIN / LAUNCHER intent filter may or may not match the implicit MAIN / LAUNCHER intent sent via startActivity (...)
+        // The startActivity (Intent) call does not mean “Start an activity corresponding to this implicit intent.”
+        // It means “Start the DEFAULT activity corresponding to this implicit intent”.
+
+        // When you send an implicit intent using startActivity (...) (or startActivityForResult (...)),
+        // the OS invisibly includes in intent the Intent category CATEGORY_DEFAULT.
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        // intent filters of MAIN / LAUNCHER may not include CATEGORY_DEFAULT, the reliability of their compliance with implicit intents is not guaranteed.
+        // Therefore, we use the intent for a direct request from the PackageManager for information about activities with filter MAIN / LAUNCHER.
+        PackageManager pm = getActivity().getPackageManager();
+        // Activity labels along with other metadata are contained in ResolveInfo objects returned by the PackageManager.
+        // these activities are launcher activities, so the label should be the name of the application.
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                // sorting ResolveInfo objects returned by the PackageManager in alphabetical order of
+                // the labels obtained by the ResolveInfo.loadLabel (...) method.
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                    a.loadLabel(pm).toString(),
+                    b.loadLabel(pm).toString());
+            }
+        });
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+
+    private class ActivityHolder extends RecyclerView.ViewHolder {
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+
+        public ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView) itemView;
+        }
+
+        public void bindActivity(ResolveInfo pResolveInfo){
+            mResolveInfo = pResolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+            mNameTextView.setText(appName);
+        }
+    }
+
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+        private final List<ResolveInfo> mActivities;
+
+        private ActivityAdapter(List<ResolveInfo> pActivities) {
+            mActivities = pActivities;
+        }
+
+
+        @NonNull
+        @Override
+        public ActivityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
     }
 }
